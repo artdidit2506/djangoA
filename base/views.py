cfa3209875c1b877af02aada256db13efe3e6de9
artdidit2506 <artdidit2506@gmail.com>
2022-04-16 22:37:49 +0700

    update models.py dan views.py

--- base/views.py
+++ base/views.py
@@ -1,25 +1,28 @@
 from multiprocessing import context
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Lets learn django'},
-    {'id': 3, 'name': 'Lets learn flask'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python'},
+#     {'id': 2, 'name': 'Lets learn django'},
+#     {'id': 3, 'name': 'Lets learn flask'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {
         'rooms': rooms,
     }
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+    # room = None
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
     context = {
         'room': room,
     }
