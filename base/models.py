907e490e92df543dc626287fabae786e50caa1d9
artdidit2506 <artdidit2506@gmail.com>
2022-04-16 21:46:47 +0700

    include migrtioans dan migrate di models.py

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
+from turtle import update
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    # host = models.ForeignKey(User, on_delete=models.CASCADE)
+    # topic = models.CharField(max_length=200)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    # participants = models.ManyToManyField(User, related_name='participants')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
