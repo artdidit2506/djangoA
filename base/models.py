13cefbd6e3eb69a606ced12fc7806cae937b549c
artdidit2506 <artdidit2506@gmail.com>
2022-04-16 22:56:11 +0700

    midifikasi di model.py

--- base/models.py
+++ base/models.py
@@ -1,10 +1,19 @@
 from turtle import update
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Topic(models.Model):
+    # room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    # description = models.TextField(null=True, blank=True)
+    # updated = models.DateTimeField(auto_now=True)
+    # created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
 class Room(models.Model):
-    # host = models.ForeignKey(User, on_delete=models.CASCADE)
-    # topic = models.CharField(max_length=200)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     # participants = models.ManyToManyField(User, related_name='participants')
@@ -12,3 +21,14 @@ class Room(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
+    
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.body[0:50]
