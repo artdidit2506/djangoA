114e8687e70f4916ca89a92f078d2b61c4570be7
artdidit2506 <artdidit2506@gmail.com>
2022-04-17 22:37:32 +0700

    setting login

--- base/views.py
+++ base/views.py
@@ -100,8 +100,10 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    messages = room.message_set.all()
     context = {
         'room': room,
+        'messages': messages,
     }
     return render(request, 'base/room.html', context)
 
