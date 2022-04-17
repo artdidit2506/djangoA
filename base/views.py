823a56087cdd4679fb58820acddc9ade2c0ca213
artdidit2506 <artdidit2506@gmail.com>
2022-04-18 00:45:43 +0700

    setting delete message

--- base/views.py
+++ base/views.py
@@ -92,6 +92,7 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body'),
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
     context = {
         'room': room,
@@ -143,5 +144,20 @@ def deleteRoom(request, pk):
         return redirect('home')
     context = {
         'obj': room,
+    }
+    return render(request, 'base/delete.html', context)
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    context = {
+        'obj': message,
     }
     return render(request, 'base/delete.html', context)
